53641d0c40b867732135e4a80a79871a06f5962e
Chetan Dhanvij <Chetan.Dhanvij@pfizer.com>
2018-02-26 01:34:43 +0530

    production

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,9 +1,23 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyArmgUaQ9V71GAOytKoC-uY62Azdr5CRWc",
-    authDomain: "fir-f1af3.firebaseapp.com",
-    databaseURL: "https://fir-f1af3.firebaseio.com",
-    projectId: "fir-f1af3",
-    storageBucket: "fir-f1af3.appspot.com",
-    messagingSenderId: "587943925065"
+//Development
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyArmgUaQ9V71GAOytKoC-uY62Azdr5CRWc",
+//     authDomain: "fir-f1af3.firebaseapp.com",
+//     databaseURL: "https://fir-f1af3.firebaseio.com",
+//     projectId: "fir-f1af3",
+//     storageBucket: "fir-f1af3.appspot.com",
+//     messagingSenderId: "587943925065"
+//   };
+
+
+  //Production
+  export const firebaseConfig = {
+    apiKey: "AIzaSyAps0_ogPxV1MPYwRFNr0OuOMtyJZf0qyA",
+    authDomain: "mikunbi-7f02f.firebaseapp.com",
+    databaseURL: "https://mikunbi-7f02f.firebaseio.com",
+    projectId: "mikunbi-7f02f",
+    storageBucket: "mikunbi-7f02f.appspot.com",
+    messagingSenderId: "770779745914"
   };
+
+
   
